070776fe909b8e2033ca41bdcc631510b9cd17b0
JuliaMuk <mukhina_ur@mail.ru>
2025-05-29 12:02:11 +0500

    create tasker

---
+++ js/tasker.js
@@ -0,0 +1,29 @@
+const container = document.querySelector(".container");
+const btnCreat = document.getElementById("btn-create-task");
+const newTask = document.getElementById("new-task");
+
+let tasks = document.querySelectorAll(".task");
+
+
+
+
+
+//добавление новой задачи
+btnCreat.addEventListener("click", function(event){
+    event.preventDefault();//отменяем повдение по умолчанию - форма не обнавляет страницу
+    let text = newTask.value;
+    const task = document.createElement("div");
+    task.classList.add("task");
+    task.innerHTML = `<span>${text}</span>`;
+    container.append(task);
+    tasks = document.querySelectorAll(".task");
+    console.log(tasks);
+});
+
+//удаление всех задач
+const btnRemove = document.getElementById("btn-remove");
+btnRemove.addEventListener("click", function(){
+    container.innerHTML = "";
+    tasks = document.querySelectorAll(".task");
+    console.log(tasks);
+});
